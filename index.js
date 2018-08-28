4ebd477c26ad5ab167b6a25bf7369321b0c81c0f
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-28 00:23:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 class Neighborhood {
-  constructor(name) {
-    this.name = name;
-    this.id = neighborhoodId++;
-  }
+  let neighborhoodIds = 1;
+    constructor(name) {
+      this.id = neighborhoodIds++;
+      this.name = name;
+      store.neighborhoods.push(this);
+    }
 }
