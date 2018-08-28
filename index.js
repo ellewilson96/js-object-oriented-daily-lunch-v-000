32d08d8a890dd30707c256ef3194293996e8ed8a
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-28 00:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+class Neighborhood {
+  constructor(name) {
+    this.name = name;
+    this.id = neighborhoodId++;
+  }
+}
